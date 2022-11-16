64ad2fb2439c4328605adcc485995dcdee7700ce
MinuSong2022 <smw9885464@naver.com>
2022-11-16 14:54:58 +0900

    221116 6교시 시작

---
+++ 221116/colors.js
@@ -0,0 +1,70 @@
+
+var Body = {
+ setColor: function(color) {
+     $('body').css('color', color);
+    //  document.querySelector('body').style.color = color;
+},
+ setBackgroundColor: function(color) {
+    $('body').css('backgroundcolor', color);
+//    document.querySelector('body').style.backgroundColor = color;
+// }
+}
+
+var Links = {
+ setColor: function(color) {
+  var alist = document.querySelectorAll('a');
+  var i = 0;
+  while(i < alist.length) {
+  alist[i].style.color = color;
+  i = i + 1;
+  }
+  }
+  }
+
+
+// function BodySetColor(color) {
+// document.querySelector('body').style.color = color;
+// }
+
+//   function setColor(color) {
+// var alist = document.querySelectorAll('a');
+// var i = 0;
+// while ( i < alist.length) {
+// 		alist[i].style.color = color;
+// 		console.log(alist[i]);
+// 		i = i + 1;
+// 	}
+// }
+
+  function nightDayHandler(self) { //함수를 선언 하는 위치 : 선언부 , 매개변수 
+// 함수를 구현하는 위치. : 구현부
+// 함수를 사용할 때 보통 " 호출했다. call" -> 함수를 사용할 때, 호출할 때 안에 있는 변수를
+// 인자값 (argument) 
+ 
+var target = document.querySelector('body');
+        if (self.value === 'night') {
+           // target.style.backgroundColor = 'black';
+            // BodySetBackgroudSetColor('black')
+            Body.setBackgroudColor('black');
+           // target.style.color = 'white';
+           Body.setColor('white')
+            self.value = 'day'
+            // setColor('powderblue');
+            Links.setColor('powderblue');
+
+        
+        } else {
+            //target.style.backgroundColor = 'white';
+            // BodySetBackgroudSetColor('white')
+            Body.SetBackgroudSetColor('white');
+           // target.style.color = 'black';
+            BodySetColor('black');
+            self.value = 'night'
+        
+            Links.setColor('powderblue');
+
+
+
+            }
+        }
+    
